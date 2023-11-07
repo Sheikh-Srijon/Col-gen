2d932bda3d0fc66f4d921859d66cdccace4818c0
Srijon <sheikhsrijon@gmail.com>
2023-11-07 03:42:29 -0800

    adding barebones frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import FeedbackForm from './data-generation/feedbackForm';
+function App() {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <FeedbackForm />
+    </div>
+  );
+}
+export default App;
